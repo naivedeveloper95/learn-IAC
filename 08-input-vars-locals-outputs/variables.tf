a05755eaceafd217d42afe284ec1d368aa4002e8
Satyam Suman <findsatyam95@gmail.com>
2026-01-04 16:15:23 +0530

    Playing around with different variable types

--- 08-input-vars-locals-outputs/variables.tf
+++ 08-input-vars-locals-outputs/variables.tf
@@ -3,3 +3,34 @@ variable "aws_region" {
   type        = string
   default     = "ap-south-1"
 }
+
+variable "ec2_instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "The type of managed EC2 instances."
+
+  validation {
+    condition     = contains(["t2.micro", "t3.micro", "t4.micro"], var.ec2_instance_type)
+    error_message = "Invalid instance type. Allowed types are: t2.micro, t3.micro, t4.micro."
+  }
+}
+
+variable "ec2_volume_config" {
+  type = map(object({
+    size = number
+    type = string
+  }))
+  default = {
+    config = {
+      size = 10
+      type = "gp3"
+    }
+  }
+  description = "The size and type in GB of the EC2 root volume."
+}
+
+variable "ec2_volume_type" {
+  type        = string
+  default     = "gp3"
+  description = "The type of the EC2 root volume."
+}
